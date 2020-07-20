7e51509971aa22a6308e56d7a8daaa9cd4b42d26
zTsugumi <nguyenngochai.tin47@gmail.com>
2020-07-21 01:30:28 +0300

    Add some minor code to schedule-class.lisp

--- src/schedule-class.lisp
+++ src/schedule-class.lisp
@@ -19,4 +19,28 @@
 (defvar *classrooms* (load-table 'classrooms))
 (defvar *subjects* (load-table 'subjects))
 (defvar *classes* (load-table 'classes))
-(defvar *faculties_subjects* (load-table 'faculty_subjects))
+(defvar *faculties_subjects* (load-table 'faculty_subject))
+
+(defvar *day-names*
+  '("Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat"))
+
+(defun classes-get-day-list ()
+  "From *classes* convert date to day of week"
+  (mapcar
+   (lambda (class)
+     (nth (local-time:timestamp-day-of-week (nth 3 class)) *day-names*))
+   *classes*)
+)
+
+(defun solve ()
+  "Loop through every classrooms, check at what time the room is free based on classes schedule"
+  (loop for classroom in *classrooms*
+     do
+       (multiple-value-bind (id room-nr start-time para) (values-list classroom)
+	 (format t "id: ~a" id)
+	 (loop for class in *classes*
+	    do
+	      (let ((classroom-id (nth 1 class)))
+		(if (eql id classroom-id) (print class))))
+	 (format t "~%~%")))
+)
