922e837f232e25876986373cd527d610cc1c0055
Klejdi Jatagani <klejdi.jatagani@yahoo.com>
2020-07-22 15:39:17 +0300

    Add trigger function on classrooms table

--- db/database.sql
+++ db/database.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE schedule_db;
+--CREATE DATABASE schedule_db;
 
 \c schedule_db;
 
 DROP TABLE IF EXISTS faculty_subject;
 DROP TABLE IF EXISTS classes;
-DROP TABLE IF EXISTS groups;
-DROP TABLE IF EXISTS faculties;
-DROP TABLE IF EXISTS subjects;
-DROP TABLE IF EXISTS classrooms;
+DROP TABLE IF EXISTS groups CASCADE;
+DROP TABLE IF EXISTS faculties CASCADE;
+DROP TABLE IF EXISTS subjects CASCADE;
+DROP TABLE IF EXISTS classrooms CASCADE;
+
 
 CREATE TABLE faculties(
 	id serial PRIMARY KEY,
@@ -47,6 +48,7 @@ CREATE TABLE classes(
 	class_type VARCHAR(10) NOT NULL
 );
 
+
 ALTER TABLE faculties
 	ADD CONSTRAINT unique_faculity_name UNIQUE (name, course);
 
@@ -65,9 +67,39 @@ ALTER TABLE classes
 	ADD CONSTRAINT fkey_classroom_id FOREIGN KEY (classroom_id) REFERENCES classrooms(id),
 	ADD CONSTRAINT fkey_group_id FOREIGN KEY (group_id) REFERENCES groups(id);
 
--- PARA = 90 min; BREAK = 10 min
--- ALTER TABLE classrooms ADD CONSTRAINT para_check CHECK (para BETWEEN 1 AND 7);
+--ALTER TABLE classrooms ADD CONSTRAINT para_check CHECK (para BETWEEN 1 AND 7);
 ALTER TABLE classrooms ADD CONSTRAINT time_check CHECK (start_time >= '{"8:30"}');
+
+
+CREATE VIEW subject_groups_view as
+	SELECT subject_id, groups.id as group_id
+	FROM faculty_subject as fc
+	INNER JOIN groups USING (faculty_id)
+	ORDER BY subject_id ASC;
+
+	
+CREATE OR REPLACE FUNCTION check_group_availability() 
+RETURNS trigger AS $check_group_availability$
+    BEGIN
+        -- Check that group_id exists for the given subject_id
+        IF NEW.group_id NOT IN
+		(	
+			SELECT	group_id
+			FROM 	subject_groups_view as sg_view
+			WHERE 	sg_view.subject_id = NEW.subject_id
+		)
+        THEN
+			RAISE EXCEPTION 'Subject does not belong to group';
+        END IF;
+		RETURN NEW;
+    END; $check_group_availability$ 
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER check_group_availability
+BEFORE INSERT OR UPDATE ON classes
+FOR EACH ROW 
+EXECUTE PROCEDURE check_group_availability();
 
 
 -- INSERT DATA FROM CSV FILE
@@ -80,16 +112,7 @@ ALTER TABLE classrooms ADD CONSTRAINT time_check CHECK (start_time >= '{"8:30"}'
 
 
 -- INSERT DATA MANUALLY 
--- INSERT INTO classrooms (room_nr, start_time, para)
--- VALUES
--- 	(
--- 		511,
--- 		'{"8:30", "10:10", "8:30", "11:50", "8:30", "10:10"}',
--- 		'{4, 2, 5, 2, 3, 5}'
--- 	);
-
--- INSERT INTO faculties (name, subject_list)
--- VALUES
--- 	('SM', '{"Programming in C", "Logic and Theory Algorithms", "Data Structure", "DBMS", "Computer Design"}'),
--- 	('LR', '{"Probability Theory", "Advanced Mathematics", "Discrete mathematics", "Decision Theory", "Mathematical statistics"}'),
--- 	('EN', '{"Physics", "Mechatronics System", "Electric drive of machines", "Analysis of machine structures", "Basics of machine tools development"}');
+--	INSERT INTO classes (subject_id, classroom_id, group_id, day, start_time, class_type)
+--	VALUES 
+--		(1, 4, 1, '2020-07-13', '8:30', 'seminar'); 	-- successfuly pass check_group_availability trigger
+--		(1, 1, 18, '2020-07-13', '8:30', 'seminar'); 	-- raise exception when subject_id doesn't belong to group_id
